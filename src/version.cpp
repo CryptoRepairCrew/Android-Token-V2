3fca996c0f676ca3cb6f3512497210d81deb5133
ahmedbodi <ahmed_bodi@msn.com>
2014-02-22 13:49:51 +0000

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -8,11 +8,11 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("AndroidsTokens");
 
 // Client version number
 // Refer to version.h for current version number
-#define CLIENT_VERSION_SUFFIX   "-cgb"
+#define CLIENT_VERSION_SUFFIX   "-ADTv2"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
